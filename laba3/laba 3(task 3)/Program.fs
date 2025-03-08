1f72c0689975097a2826bdf221b4242d8a7fcb8c
vlad896-ug <vlados90ip@mail.ru>
2025-03-08 19:28:07 +0500

    Add files via upload

---
+++ laba3/laba 3(task 3)/Program.fs
@@ -0,0 +1,18 @@
+﻿open System
+open System.IO
+
+[<EntryPoint>]
+let main argv =
+    let directory = "E:\\labafile"
+    let rec getFiles d =
+        seq {
+            yield! Directory.EnumerateFiles(d) 
+        }
+    let files = getFiles directory
+    let sortedFiles = files |> Seq.sort
+    match Seq.tryHead sortedFiles with
+    | Some firstFile ->
+        printfn "Первый файл по алфавиту: %s" (Path.GetFileName firstFile)
+    | None ->
+        printfn "В каталоге '%s' нет файлов." directory
+    0 
