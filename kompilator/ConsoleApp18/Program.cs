6b466d7f49e7326cdb0f89d6503f7f66fa24efdb
vlad896-ug <vlados90ip@mail.ru>
2025-05-30 00:58:10 +0500

    Add files via upload

---
+++ kompilator/ConsoleApp18/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Работает Pascal-компилятор");
+        string inputPath = "D:\\Влад\\пгниу\\языки программирования\\компилятор\\pascal_tests\\test4.txt";
+        if (!File.Exists(inputPath))
+        {
+            Console.WriteLine($"Файл {inputPath} не найден.");
+            return;
+        }
+        List<string> inputLines = new List<string>(File.ReadAllLines(inputPath));
+        InputOutput io = new InputOutput(inputLines);
+        LexicalAnalyzer lexer = new LexicalAnalyzer(io);
+        lexer.Analyze();
+        Console.WriteLine($"\nКомпиляция завершена: ошибок — {io.TotalErrors}!");
+    }
+}
+
+
+
+
+
